f365df45d0b2d3d10ae1743a3fe838e5fb0e6e8a
Aswin K S <aswinkalathur11@gmail.com>
2025-11-17 23:56:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import morgan from 'morgan';
+
+
+const app=express();
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to Node.js Backend API',
+    version: '1.0.0',
+   
+  });
+});
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+})
